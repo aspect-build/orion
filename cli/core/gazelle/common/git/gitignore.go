e53070aea65e6d93eeea21db08f2a71783ce9f07
Jason Bedard <jason@aspect.dev>
2023-06-06 18:14:16 -0700

    refactor(gazelle): move common code into gazelle/common (#2458)

---
+++ cli/core/gazelle/common/git/gitignore.go
@@ -0,0 +1,82 @@
+package git
+
+import (
+	"os"
+	"path"
+	"strings"
+
+	. "github.com/aspect-build/silo/cli/core/gazelle/common/log"
+	"github.com/bazelbuild/bazel-gazelle/config"
+	gitignore "github.com/sabhiram/go-gitignore"
+)
+
+// Ignore files following .gitignore syntax for files gazelle will ignore.
+var bazelIgnoreFiles = []string{".bazelignore", ".gitignore"}
+
+type GitIgnore struct {
+	ignores map[string][]*gitignore.GitIgnore
+}
+
+func NewGitIgnore() *GitIgnore {
+	return &GitIgnore{
+		ignores: make(map[string][]*gitignore.GitIgnore),
+	}
+}
+
+func (i *GitIgnore) CollectIgnoreFiles(c *config.Config, rel string) {
+	// Collect gitignore style ignore files in this directory.
+	for _, ignoreFileName := range bazelIgnoreFiles {
+		ignoreRelPath := path.Join(rel, ignoreFileName)
+		ignoreFilePath := path.Join(c.RepoRoot, ignoreRelPath)
+
+		if _, ignoreErr := os.Stat(ignoreFilePath); ignoreErr == nil {
+			BazelLog.Tracef("Add ignore file %s", ignoreRelPath)
+
+			ignoreErr := i.AddIgnoreFile(rel, ignoreFilePath)
+			if ignoreErr != nil {
+				BazelLog.Fatalf("Failed to add ignore file %s: %v", ignoreRelPath, ignoreErr)
+			}
+		}
+	}
+}
+
+// Add the given ignore file rules relative to the given directory.
+func (i *GitIgnore) AddIgnoreFile(ignoreDir, ignoreFile string) error {
+	contents, err := os.ReadFile(ignoreFile)
+	if err != nil {
+		return err
+	}
+
+	i.addIgnoreFileContent(ignoreDir, strings.Split(string(contents), "\n"))
+	return nil
+}
+
+func (i *GitIgnore) addIgnoreFileContent(ignoreDir string, ignoreContents []string) {
+	ignoreDir = path.Clean(ignoreDir)
+
+	if i.ignores[ignoreDir] == nil {
+		i.ignores[ignoreDir] = make([]*gitignore.GitIgnore, 0)
+	}
+
+	ignore := gitignore.CompileIgnoreLines(ignoreContents...)
+
+	i.ignores[ignoreDir] = append(i.ignores[ignoreDir], ignore)
+}
+
+func (i *GitIgnore) Matches(p string) bool {
+	p = path.Clean(p)
+
+	for d, f := path.Dir(p), path.Base(p); ; d, f = path.Dir(d), path.Join(path.Base(d), f) {
+		if ignores := i.ignores[d]; ignores != nil {
+			for _, ignore := range ignores {
+				if ignore.MatchesPath(f) {
+					return true
+				}
+			}
+		}
+
+		if d == "." {
+			return false
+		}
+	}
+}
---
+++ cli/core/gazelle/common/walk.go
@@ -0,0 +1,68 @@
+package gazelle
+
+import (
+	"os"
+	"path"
+	"path/filepath"
+
+	. "github.com/aspect-build/silo/cli/core/gazelle/common/log"
+	"github.com/bazelbuild/bazel-gazelle/language"
+)
+
+type GazelleWalkFunc func(path string) error
+
+// Walk the directory of the language.GenerateArgs, optionally recursing into
+// subdirectories unlike the files provided in GenerateArgs.RegularFiles.
+func GazelleWalkDir(args language.GenerateArgs, recurse bool, walkFunc GazelleWalkFunc) error {
+	BazelLog.Tracef("GazelleWalkDir: %s", args.Rel)
+
+	// Source files in the primary directory
+	for _, f := range args.RegularFiles {
+		BazelLog.Tracef("GazelleWalkDir RegularFile: %s", f)
+
+		walkErr := walkFunc(f)
+		if walkErr != nil && walkErr != filepath.SkipDir {
+			return walkErr
+		}
+	}
+
+	// Do not manually traverse Subdirs unless specified
+	if !recurse {
+		return nil
+	}
+
+	// Source files throughout the sub-directories of this BUILD.
+	for _, d := range args.Subdirs {
+		err := filepath.Walk(
+			path.Join(args.Dir, d),
+			func(filePath string, info os.FileInfo, err error) error {
+				if err != nil {
+					return err
+				}
+
+				// If we are visiting a directory recurse if it is not a bazel package.
+				if info.IsDir() && IsBazelPackage(filePath) {
+					return filepath.SkipDir
+				}
+
+				// Skip BUILD files
+				if isBuildFile(filePath) {
+					return nil
+				}
+
+				// The filePath relative to the BUILD
+				f, _ := filepath.Rel(args.Dir, filePath)
+
+				BazelLog.Tracef("GazelleWalkDir Subdir file: %s", f)
+
+				return walkFunc(f)
+			},
+		)
+
+		if err != nil && err != filepath.SkipDir {
+			return err
+		}
+	}
+
+	return nil
+}
