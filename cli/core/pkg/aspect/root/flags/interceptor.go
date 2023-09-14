aspect-build/orion

commit ab6228d29b0a0f54b3c753d189d7bdbec48952e5

fix: fixup aspect cli global flag parsing on bazel commands (#3368)
This came out of working on the build event stream forwarding proxy when
the user specifies a `--bes_backend` and we have plugins enabled.

We've gone in circles with cobra bazel flag parsing so many times. This
looks correct with all bazel flags being registered and undocumented
ones marked as hidden. Flags interceptor parses all flags before `--`
(if it exists) and forwards all non-aspect flags onto the bazel command.

This fixes an issue where aspect global flags were _not_ parsed on bazel
commands. It also fixes an issue that undocumented bazel flags were
_not_ added to commands and not parsed which made getting the values of
user supplied bazel flags such as `--bes_backend` and `--remote_header`
was non-trivial.

---

### Type of change

- Refactor (a code change that neither fixes a bug or adds a new
feature)

### Test plan

- Covered by existing test cases

cli/core/pkg/aspect/root/flags/interceptor.go
@@ -18,105 +18,41 @@ package flags
 
 import (
 	"context"
-	"fmt"
 	"strings"
 
 	"github.com/spf13/cobra"
-	"github.com/spf13/pflag"
 
 	"github.com/aspect-build/silo/cli/core/pkg/interceptors"
 	"github.com/aspect-build/silo/cli/core/pkg/ioutils"
 )
 
-// AddGlobalFlags will add Aspect specific flags to all cobra commands.
-func AddGlobalFlags(cmd *cobra.Command, defaultInteractive bool) {
-	cmd.PersistentFlags().Bool(AspectLockVersion, false, "Lock the version of the Aspect CLI. This prevents the Aspect CLI from downloading and running an different version of the Aspect CLI if one is specified in .bazeliskrc or the Aspect CLI config.")
-	cmd.PersistentFlags().MarkHidden(AspectLockVersion)
-
-	cmd.PersistentFlags().String(AspectConfigFlagName, "", fmt.Sprintf("User-specified Aspect CLI config file. /dev/null indicates that all further --%s flags will be ignored.", AspectConfigFlagName))
-
-	RegisterNoableBool(cmd.PersistentFlags(), AspectSystemConfigFlagName, true, "Whether or not to look for the system config file at /etc/aspect/cli/config.yaml")
-	cmd.PersistentFlags().MarkHidden(AspectSystemConfigFlagName)
-	cmd.PersistentFlags().MarkHidden(NoFlagName(AspectSystemConfigFlagName))
-
-	RegisterNoableBool(cmd.PersistentFlags(), AspectWorkspaceConfigFlagName, true, "Whether or not to look for the workspace config file at $workspace/.aspect/cli/config.yaml")
-	cmd.PersistentFlags().MarkHidden(AspectWorkspaceConfigFlagName)
-	cmd.PersistentFlags().MarkHidden(NoFlagName(AspectWorkspaceConfigFlagName))
-
-	RegisterNoableBool(cmd.PersistentFlags(), AspectHomeConfigFlagName, true, "Whether or not to look for the home config file at $HOME/.aspect/cli/config.yaml")
-	cmd.PersistentFlags().MarkHidden(AspectHomeConfigFlagName)
-	cmd.PersistentFlags().MarkHidden(NoFlagName(AspectHomeConfigFlagName))
-
-	cmd.PersistentFlags().Bool(AspectInteractiveFlagName, defaultInteractive, "Interactive mode (e.g. prompts for user input)")
-}
-
-func extractUnknownArgs(flags *pflag.FlagSet, args []string) []string {
-	unknownArgs := []string{}
-
-	for i := 0; i < len(args); i++ {
-		arg := args[i]
-		var flag *pflag.Flag
-		if arg[0] == '-' {
-			if arg[1] == '-' {
-				if len(arg) == 2 { // "--" terminates the flags
-					unknownArgs = append(unknownArgs, args[i:]...)
-					break
-				}
-				flag = flags.Lookup(strings.SplitN(arg[2:], "=", 2)[0])
-			} else {
-				for _, s := range arg[1:] {
-					flag = flags.ShorthandLookup(string(s))
-					if flag == nil {
-						break
-					}
-				}
-			}
-		}
-		if flag != nil {
-			if flag.NoOptDefVal == "" && i+1 < len(args) && flag.Value.String() == args[i+1] {
-				i++
-			}
-			continue
-		}
-		unknownArgs = append(unknownArgs, arg)
-	}
-	return unknownArgs
-}
-
-// FlagsInterceptor will parse the incoming flags and remove any aspect specific flags or bazel
-// startup flags from the list of args.
 func FlagsInterceptor(streams ioutils.Streams) interceptors.Interceptor {
-	return flagsInterceptor(streams, true)
-}
-
-// FlagsInterceptorWithoutParser will remove any aspect specific flags or bazel startup flags from the list of args.
-func FlagsInterceptorWithoutParser(streams ioutils.Streams) interceptors.Interceptor {
-	return flagsInterceptor(streams, false)
-}
-
-func flagsInterceptor(streams ioutils.Streams, parse bool) interceptors.Interceptor {
 	return func(ctx context.Context, cmd *cobra.Command, args []string, next interceptors.RunEContextFn) error {
-
+		// DisableFlagParsing is true for commands that call out to Bazel. When DisableFlagParsing is true,
+		// FlagsInterceptor will parse all flags but only remove aspect specific `--aspect:“ flags so that
+		// all bazel flags can be forward to the bazel command.
 		if cmd.DisableFlagParsing {
-			args = extractUnknownArgs(cmd.InheritedFlags(), args)
-
-			if parse {
-				cmd.DisableFlagParsing = false
-				if err := cmd.ParseFlags(args); err != nil {
-					return err
+			parseArgs := make([]string, 0, len(args))
+			forwardArgs := make([]string, 0, len(args))
+			doubleDash := false
+			for _, arg := range args {
+				if arg == "--" {
+					doubleDash = true
+				}
+				if !doubleDash {
+					parseArgs = append(parseArgs, arg)
+				}
+				if doubleDash || !strings.HasPrefix(arg, "--aspect:") {
+					forwardArgs = append(forwardArgs, arg)
 				}
 			}
-		}
-
-		for _, arg := range args {
-			if arg == "--" {
-				break
+			cmd.DisableFlagParsing = false
+			if err := cmd.ParseFlags(parseArgs); err != nil {
+				return err
 			}
-			if strings.HasPrefix(arg, "--aspect:") {
-				return fmt.Errorf("unknown flag: %s", arg)
-			}
+			cmd.DisableFlagParsing = true
+			args = forwardArgs
 		}
-
 		return next(ctx, cmd, args)
 	}
 }

cli/core/pkg/aspect/outputs/outputs.go
@@ -44,11 +44,6 @@ func (runner *Outputs) Run(_ context.Context, _ *cobra.Command, args []string) e
 		return err
 	}
 
-	// Strip any leading -- that can come from ParseOutBazelFlags
-	if len(nonFlags) > 0 && nonFlags[0] == "--" {
-		nonFlags = nonFlags[1:]
-	}
-
 	if len(nonFlags) < 1 {
 		return fmt.Errorf("a query expression is required as the first argument to outputs command")
 	}

cli/core/pkg/aspect/root/flags/BUILD.bazel
@@ -4,6 +4,7 @@ go_library(
     name = "flags",
     srcs = [
         "aspect_flags.go",
+        "global.go",
         "interceptor.go",
         "multi_string.go",
         "noable_bool.go",

cli/core/pkg/aspect/root/flags/global.go
@@ -1,0 +1,45 @@
+/*
+ * Copyright 2022 Aspect Build Systems, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package flags
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func AddGlobalFlags(cmd *cobra.Command, defaultInteractive bool) {
+	// Documented global flags. These flags show up as "global flags" on the `help` command output.
+	cmd.PersistentFlags().String(AspectConfigFlagName, "", fmt.Sprintf("User-specified Aspect CLI config file. /dev/null indicates that all further --%s flags will be ignored.", AspectConfigFlagName))
+	cmd.PersistentFlags().Bool(AspectInteractiveFlagName, defaultInteractive, "Interactive mode (e.g. prompts for user input)")
+
+	// Hidden global flags
+	cmd.PersistentFlags().Bool(AspectLockVersion, false, "Lock the version of the Aspect CLI. This prevents the Aspect CLI from downloading and running an different version of the Aspect CLI if one is specified in .bazeliskrc or the Aspect CLI config.")
+	cmd.PersistentFlags().MarkHidden(AspectLockVersion)
+
+	RegisterNoableBool(cmd.PersistentFlags(), AspectSystemConfigFlagName, true, "Whether or not to look for the system config file at /etc/aspect/cli/config.yaml")
+	cmd.PersistentFlags().MarkHidden(AspectSystemConfigFlagName)
+	cmd.PersistentFlags().MarkHidden(NoFlagName(AspectSystemConfigFlagName))
+
+	RegisterNoableBool(cmd.PersistentFlags(), AspectWorkspaceConfigFlagName, true, "Whether or not to look for the workspace config file at $workspace/.aspect/cli/config.yaml")
+	cmd.PersistentFlags().MarkHidden(AspectWorkspaceConfigFlagName)
+	cmd.PersistentFlags().MarkHidden(NoFlagName(AspectWorkspaceConfigFlagName))
+
+	RegisterNoableBool(cmd.PersistentFlags(), AspectHomeConfigFlagName, true, "Whether or not to look for the home config file at $HOME/.aspect/cli/config.yaml")
+	cmd.PersistentFlags().MarkHidden(AspectHomeConfigFlagName)
+	cmd.PersistentFlags().MarkHidden(NoFlagName(AspectHomeConfigFlagName))
+}

cli/core/pkg/bazel/bazel_flags.go
@@ -67,7 +67,7 @@ var (
 	bazelFlagSets = map[string]*pflag.FlagSet{}
 )
 
-func addFlagToFlagSet(flag *flags.FlagInfo, flagSet *pflag.FlagSet) {
+func addFlagToFlagSet(flag *flags.FlagInfo, flagSet *pflag.FlagSet, hidden bool) {
 	flagName := flag.GetName()
 	flagAbbreviation := flag.GetAbbreviation()
 	flagDoc := flag.GetDocumentation()
@@ -84,7 +84,9 @@ func addFlagToFlagSet(flag *flags.FlagInfo, flagSet *pflag.FlagSet) {
 			flagSet.StringP(flagName, flagAbbreviation, "", flagDoc)
 		}
 	}
-
+	if hidden {
+		flagSet.MarkHidden(flagName)
+	}
 }
 
 // InitializeBazelFlags will create FlagSets for each bazel command (including
@@ -106,7 +108,7 @@ func (b *bazel) InitializeBazelFlags() error {
 				flagSet = pflag.NewFlagSet(command, pflag.ContinueOnError)
 				bazelFlagSets[command] = flagSet
 			}
-			addFlagToFlagSet(flag, flagSet)
+			addFlagToFlagSet(flag, flagSet, true)
 		}
 	}
 	return nil
@@ -140,11 +142,8 @@ func (b *bazel) AddBazelFlags(cmd *cobra.Command) error {
 			}
 			for _, command := range commands {
 				if subcommand, ok := subCommands[command]; ok {
-					subcommand.DisableFlagParsing = true // only want to disable flag parsing on actual bazel verbs
-					subcommand.FParseErrWhitelist.UnknownFlags = true
-					if documented {
-						addFlagToFlagSet(flag, subcommand.Flags())
-					}
+					subcommand.DisableFlagParsing = true // only want to disable flag parsing on commands that call out to bazel
+					addFlagToFlagSet(flag, subcommand.Flags(), !documented)
 				}
 			}
 		}
@@ -187,7 +186,6 @@ func ParseOutBazelFlags(command string, args []string) ([]string, []string, erro
 
 		if s[1] == '-' {
 			if len(s) == 2 { // "--" terminates the flags
-				nonFlags = append(nonFlags, s)
 				nonFlags = append(nonFlags, args...)
 				break
 			}

cli/core/pkg/plugin/system/system.go
@@ -139,15 +139,12 @@ func (ps *pluginSystem) RegisterCustomCommands(cmd *cobra.Command, bazelStartupA
 			callback := node.payload.CustomCommandExecutor
 
 			cmd.AddCommand(&cobra.Command{
-				Use:                command.Use,
-				Short:              command.ShortDesc,
-				Long:               command.LongDesc,
-				GroupID:            "plugin",
-				DisableFlagParsing: true,
+				Use:     command.Use,
+				Short:   command.ShortDesc,
+				Long:    command.LongDesc,
+				GroupID: "plugin",
 				RunE: interceptors.Run(
-					[]interceptors.Interceptor{
-						rootFlags.FlagsInterceptorWithoutParser(ioutils.DefaultStreams),
-					},
+					[]interceptors.Interceptor{},
 					func(ctx context.Context, cmd *cobra.Command, args []string) (exitErr error) {
 						return callback.ExecuteCustomCommand(cmdName, ctx, args, bazelStartupArgs)
 					},

commit 8f7372ca463f5a6fd07147811b718efc474fdf61

fix: don't error out on unknown flags on commands that forward args to Bazel (#3491)
Fixes alternate bazel flag parsing regression in #3368.

---

### Type of change

- Bug fix (change which fixes an issue)

**For changes visible to end-users**

- Breaking change (this change will force users to change their own code
or config)
- Relevant documentation has been updated
- Suggested release notes are provided below:

Aspect CLI will now forward flags not reported by `flags-as-proto` to
bazel instead of erroring out. This fixes a regression where alternate
flag names which are not reported by `flags-as-proto` from bazel were
error out in the Aspect CLI when they would have been handled by bazel.

### Test plan

- Covered by existing test cases
- Manual testing; please provide instructions so we can reproduce:

Tested that unknown bazel flags passed to Aspect CLI command that call
bazel now result in a bazel error instead failing in the Aspect CLI
before calling bazel.

cli/core/pkg/aspect/root/flags/interceptor.go
@@ -21,10 +21,25 @@ import (
 	"strings"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 
 	"github.com/aspect-build/silo/cli/core/pkg/interceptors"
 	"github.com/aspect-build/silo/cli/core/pkg/ioutils"
 )
+
+func isFlagInFlagSet(flags *pflag.FlagSet, arg string) bool {
+	var flag *pflag.Flag
+	if arg[0] == '-' {
+		if arg[1] == '-' {
+			flag = flags.Lookup(strings.SplitN(arg[2:], "=", 2)[0])
+		} else {
+			for _, s := range arg[1:] {
+				flag = flags.ShorthandLookup(string(s))
+			}
+		}
+	}
+	return flag != nil
+}
 
 func FlagsInterceptor(streams ioutils.Streams) interceptors.Interceptor {
 	return func(ctx context.Context, cmd *cobra.Command, args []string, next interceptors.RunEContextFn) error {
@@ -42,11 +57,23 @@ func FlagsInterceptor(streams ioutils.Streams) interceptors.Interceptor {
 				if !doubleDash {
 					parseArgs = append(parseArgs, arg)
 				}
-				if doubleDash || !strings.HasPrefix(arg, "--aspect:") {
+				// Forward all flags to the bazel command except valid --aspect: flags that come before any
+				// double dash (--). The valid --aspect: are all in rootCmd.PersistentFlags() so we can check
+				// against that.
+				rootCmd := cmd
+				for rootCmd.Parent() != nil {
+					rootCmd = rootCmd.Parent()
+				}
+				if doubleDash || !isFlagInFlagSet(rootCmd.PersistentFlags(), arg) {
 					forwardArgs = append(forwardArgs, arg)
 				}
 			}
 			cmd.DisableFlagParsing = false
+			// Be tolerant of unknown flags since Bazel doesn't let us know what "alternate" flag names it
+			// accepts and we don't want to error out if a user passes a valid "alternate" flag such as
+			// --experimental_remote_grpc_log when the Bazel version being used reports only
+			// --remote_grpc_log as a valid flag.
+			cmd.FParseErrWhitelist = cobra.FParseErrWhitelist{UnknownFlags: true}
 			if err := cmd.ParseFlags(parseArgs); err != nil {
 				return err
 			}