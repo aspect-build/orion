aspect-build/orion

commit 92d3042b59804184736e200439390b44d97a38e0

fix: forward to --bes_backend if configured by user (#3369)
Follow-up will add parsing of `.bazelrc` and add unit tests.

---

### Type of change

- Bug fix (change which fixes an issue)

**For changes visible to end-users**

- Suggested release notes are provided below:

--bes_backend bazel cli arguments are now handled correct by Aspect CLI

### Test plan

- Covered by existing test cases
- Manual testing; please provide instructions so we can reproduce:

Lots of manual testing against BuildBuddy bes_backend

cli/core/pkg/aspect/build/build.go
@@ -48,14 +48,20 @@ func New(
 // Event Protocol backend used by Aspect plugins to subscribe to build events.
 func (runner *Build) Run(ctx context.Context, _ *cobra.Command, args []string) (exitErr error) {
 	bazelCmd := []string{"build"}
+	bazelCmd = append(bazelCmd, args...)
 
+	// Currently Bazel only supports a single --bes_backend so adding ours after
+	// any user supplied value will result in our bes_backend taking precedence.
+	// There is a very old & stale issue to add support for multiple BES
+	// backends https://github.com/bazelbuild/bazel/issues/10908. In the future,
+	// we could build this support into the Aspect CLI and post on that issue
+	// that using the Aspect CLI resolves it.
 	if bep.HasBESBackend(ctx) {
 		besBackend := bep.BESBackendFromContext(ctx)
 		besBackendFlag := fmt.Sprintf("--bes_backend=%s", besBackend.Addr())
 		bazelCmd = append(bazelCmd, besBackendFlag)
 	}
 
-	bazelCmd = append(bazelCmd, args...)
 	exitCode, bazelErr := runner.bzl.RunCommand(runner.Streams, nil, bazelCmd...)
 
 	// Process the subscribers errors before the Bazel one.

cli/core/pkg/aspect/coverage/coverage.go
@@ -41,14 +41,20 @@ func New(streams ioutils.Streams, bzl bazel.Bazel) *Coverage {
 
 func (runner *Coverage) Run(ctx context.Context, _ *cobra.Command, args []string) (exitErr error) {
 	bazelCmd := []string{"coverage"}
+	bazelCmd = append(bazelCmd, args...)
 
+	// Currently Bazel only supports a single --bes_backend so adding ours after
+	// any user supplied value will result in our bes_backend taking precedence.
+	// There is a very old & stale issue to add support for multiple BES
+	// backends https://github.com/bazelbuild/bazel/issues/10908. In the future,
+	// we could build this support into the Aspect CLI and post on that issue
+	// that using the Aspect CLI resolves it.
 	if bep.HasBESBackend(ctx) {
 		besBackend := bep.BESBackendFromContext(ctx)
 		besBackendFlag := fmt.Sprintf("--bes_backend=%s", besBackend.Addr())
 		bazelCmd = append(bazelCmd, besBackendFlag)
 	}
 
-	bazelCmd = append(bazelCmd, args...)
 	exitCode, bazelErr := runner.bzl.RunCommand(runner.Streams, nil, bazelCmd...)
 
 	// Process the subscribers errors before the Bazel one.

cli/core/pkg/aspect/run/run.go
@@ -48,14 +48,20 @@ func New(
 // Event Protocol backend used by Aspect plugins to subscribe to build events.
 func (runner *Run) Run(ctx context.Context, _ *cobra.Command, args []string) (exitErr error) {
 	bazelCmd := []string{"run"}
+	bazelCmd = append(bazelCmd, args...)
 
+	// Currently Bazel only supports a single --bes_backend so adding ours after
+	// any user supplied value will result in our bes_backend taking precedence.
+	// There is a very old & stale issue to add support for multiple BES
+	// backends https://github.com/bazelbuild/bazel/issues/10908. In the future,
+	// we could build this support into the Aspect CLI and post on that issue
+	// that using the Aspect CLI resolves it.
 	if bep.HasBESBackend(ctx) {
 		besBackend := bep.BESBackendFromContext(ctx)
 		besBackendFlag := fmt.Sprintf("--bes_backend=%s", besBackend.Addr())
 		bazelCmd = append(bazelCmd, besBackendFlag)
 	}
 
-	bazelCmd = append(bazelCmd, args...)
 	exitCode, bazelErr := runner.bzl.RunCommand(runner.Streams, nil, bazelCmd...)
 
 	// Process the subscribers errors before the Bazel one.

cli/core/pkg/aspect/build/build_test.go
@@ -47,7 +47,7 @@ func TestBuild(t *testing.T) {
 		}
 		bzl.
 			EXPECT().
-			RunCommand(streams, nil, "build", "--bes_backend=grpc://127.0.0.1:12345", "//...").
+			RunCommand(streams, nil, "build", "//...", "--bes_backend=grpc://127.0.0.1:12345").
 			Return(expectErr.ExitCode, expectErr.Err)
 		besBackend := bep_mock.NewMockBESBackend(ctrl)
 		besBackend.
@@ -78,7 +78,7 @@ func TestBuild(t *testing.T) {
 		bzl := bazel_mock.NewMockBazel(ctrl)
 		bzl.
 			EXPECT().
-			RunCommand(streams, nil, "build", "--bes_backend=grpc://127.0.0.1:12345", "//...").
+			RunCommand(streams, nil, "build", "//...", "--bes_backend=grpc://127.0.0.1:12345").
 			Return(0, nil)
 		besBackend := bep_mock.NewMockBESBackend(ctrl)
 		besBackend.
@@ -113,7 +113,7 @@ func TestBuild(t *testing.T) {
 		bzl := bazel_mock.NewMockBazel(ctrl)
 		bzl.
 			EXPECT().
-			RunCommand(streams, nil, "build", "--bes_backend=grpc://127.0.0.1:12345", "//...").
+			RunCommand(streams, nil, "build", "//...", "--bes_backend=grpc://127.0.0.1:12345").
 			Return(0, nil)
 		besBackend := bep_mock.NewMockBESBackend(ctrl)
 		besBackend.

cli/core/pkg/aspect/run/run_test.go
@@ -47,7 +47,7 @@ func TestRun(t *testing.T) {
 		}
 		bzl.
 			EXPECT().
-			RunCommand(streams, nil, "run", "--bes_backend=grpc://127.0.0.1:12345", "//...").
+			RunCommand(streams, nil, "run", "//...", "--bes_backend=grpc://127.0.0.1:12345").
 			Return(expectErr.ExitCode, expectErr.Err)
 		besBackend := bep_mock.NewMockBESBackend(ctrl)
 		besBackend.
@@ -78,7 +78,7 @@ func TestRun(t *testing.T) {
 		bzl := bazel_mock.NewMockBazel(ctrl)
 		bzl.
 			EXPECT().
-			RunCommand(streams, nil, "run", "--bes_backend=grpc://127.0.0.1:12345", "//...").
+			RunCommand(streams, nil, "run", "//...", "--bes_backend=grpc://127.0.0.1:12345").
 			Return(0, nil)
 		besBackend := bep_mock.NewMockBESBackend(ctrl)
 		besBackend.
@@ -113,7 +113,7 @@ func TestRun(t *testing.T) {
 		bzl := bazel_mock.NewMockBazel(ctrl)
 		bzl.
 			EXPECT().
-			RunCommand(streams, nil, "run", "--bes_backend=grpc://127.0.0.1:12345", "//...").
+			RunCommand(streams, nil, "run", "//...", "--bes_backend=grpc://127.0.0.1:12345").
 			Return(0, nil)
 		besBackend := bep_mock.NewMockBESBackend(ctrl)
 		besBackend.

cli/core/pkg/aspect/test/test.go
@@ -41,14 +41,20 @@ func New(streams ioutils.Streams, bzl bazel.Bazel) *Test {
 
 func (runner *Test) Run(ctx context.Context, _ *cobra.Command, args []string) (exitErr error) {
 	bazelCmd := []string{"test"}
+	bazelCmd = append(bazelCmd, args...)
 
+	// Currently Bazel only supports a single --bes_backend so adding ours after
+	// any user supplied value will result in our bes_backend taking precedence.
+	// There is a very old & stale issue to add support for multiple BES
+	// backends https://github.com/bazelbuild/bazel/issues/10908. In the future,
+	// we could build this support into the Aspect CLI and post on that issue
+	// that using the Aspect CLI resolves it.
 	if bep.HasBESBackend(ctx) {
 		besBackend := bep.BESBackendFromContext(ctx)
 		besBackendFlag := fmt.Sprintf("--bes_backend=%s", besBackend.Addr())
 		bazelCmd = append(bazelCmd, besBackendFlag)
 	}
 
-	bazelCmd = append(bazelCmd, args...)
 	exitCode, bazelErr := runner.bzl.RunCommand(runner.Streams, nil, bazelCmd...)
 
 	// Process the subscribers errors before the Bazel one.

cli/core/pkg/plugin/system/BUILD.bazel
@@ -14,6 +14,7 @@ go_library(
         "//cli/core/pkg/plugin/client",
         "//cli/core/pkg/plugin/sdk/v1alpha4/plugin",
         "//cli/core/pkg/plugin/system/bep",
+        "//cli/core/pkg/plugin/system/besproxy",
         "@com_github_spf13_cobra//:cobra",
         "@io_k8s_sigs_yaml//:yaml",
         "@org_golang_google_grpc//:go_default_library",

cli/core/pkg/plugin/system/bep/BUILD.bazel
@@ -13,10 +13,13 @@ go_library(
         "//cli/core/bazel/buildeventstream",
         "//cli/core/pkg/aspecterrors",
         "//cli/core/pkg/aspectgrpc",
+        "//cli/core/pkg/plugin/system/besproxy",
         "@go_googleapis//google/devtools/build/v1:build_go_proto",
         "@io_bazel_rules_go//proto/wkt:empty_go_proto",
         "@org_golang_google_grpc//:go_default_library",
         "@org_golang_google_grpc//credentials/insecure",
+        "@org_golang_google_protobuf//types/known/emptypb",
+        "@org_golang_x_sync//errgroup",
     ],
 )
 
@@ -28,10 +31,14 @@ go_test(
         "//cli/core/bazel/buildeventstream",
         "//cli/core/pkg/aspecterrors",
         "//cli/core/pkg/aspectgrpc/mock",
+        "//cli/core/pkg/plugin/system/besproxy",
+        "//cli/core/pkg/plugin/system/besproxy/mock",
         "//cli/core/pkg/stdlib/mock",
         "@com_github_golang_mock//gomock",
         "@com_github_onsi_gomega//:gomega",
         "@go_googleapis//google/devtools/build/v1:build_go_proto",
+        "@org_golang_google_grpc//:go_default_library",
         "@org_golang_google_protobuf//types/known/anypb",
+        "@org_golang_x_sync//errgroup",
     ],
 )

commit a152870b0fed3fc1a795781ed42b1490f108a449

fix: correctly inject --bes_backend flag before double dash if it exists (#3510)
This regressed in #3369

---

### Type of change

- Bug fix (change which fixes an issue)

### Test plan

- Covered by existing test cases
- New test cases added

cli/core/pkg/aspect/build/BUILD.bazel
@@ -6,6 +6,7 @@ go_library(
     importpath = "github.com/aspect-build/silo/cli/core/pkg/aspect/build",
     visibility = ["//visibility:public"],
     deps = [
+        "//cli/core/pkg/aspect/root/flags",
         "//cli/core/pkg/aspecterrors",
         "//cli/core/pkg/bazel",
         "//cli/core/pkg/ioutils",

cli/core/pkg/aspect/build/build.go
@@ -20,6 +20,7 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/aspect-build/silo/cli/core/pkg/aspect/root/flags"
 	"github.com/aspect-build/silo/cli/core/pkg/aspecterrors"
 	"github.com/aspect-build/silo/cli/core/pkg/bazel"
 	"github.com/aspect-build/silo/cli/core/pkg/ioutils"
@@ -59,7 +60,7 @@ func (runner *Build) Run(ctx context.Context, _ *cobra.Command, args []string) (
 	if bep.HasBESBackend(ctx) {
 		besBackend := bep.BESBackendFromContext(ctx)
 		besBackendFlag := fmt.Sprintf("--bes_backend=%s", besBackend.Addr())
-		bazelCmd = append(bazelCmd, besBackendFlag)
+		bazelCmd = flags.AddFlagToCommand(bazelCmd, besBackendFlag)
 	}
 
 	exitCode, bazelErr := runner.bzl.RunCommand(runner.Streams, nil, bazelCmd...)

cli/core/pkg/aspect/coverage/BUILD.bazel
@@ -6,6 +6,7 @@ go_library(
     importpath = "github.com/aspect-build/silo/cli/core/pkg/aspect/coverage",
     visibility = ["//visibility:public"],
     deps = [
+        "//cli/core/pkg/aspect/root/flags",
         "//cli/core/pkg/aspecterrors",
         "//cli/core/pkg/bazel",
         "//cli/core/pkg/ioutils",

cli/core/pkg/aspect/coverage/coverage.go
@@ -20,6 +20,7 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/aspect-build/silo/cli/core/pkg/aspect/root/flags"
 	"github.com/aspect-build/silo/cli/core/pkg/aspecterrors"
 	"github.com/aspect-build/silo/cli/core/pkg/bazel"
 	"github.com/aspect-build/silo/cli/core/pkg/ioutils"
@@ -52,7 +53,7 @@ func (runner *Coverage) Run(ctx context.Context, _ *cobra.Command, args []string
 	if bep.HasBESBackend(ctx) {
 		besBackend := bep.BESBackendFromContext(ctx)
 		besBackendFlag := fmt.Sprintf("--bes_backend=%s", besBackend.Addr())
-		bazelCmd = append(bazelCmd, besBackendFlag)
+		bazelCmd = flags.AddFlagToCommand(bazelCmd, besBackendFlag)
 	}
 
 	exitCode, bazelErr := runner.bzl.RunCommand(runner.Streams, nil, bazelCmd...)

cli/core/pkg/aspect/root/flags/BUILD.bazel
@@ -8,6 +8,7 @@ go_library(
         "interceptor.go",
         "multi_string.go",
         "noable_bool.go",
+        "utils.go",
     ],
     importpath = "github.com/aspect-build/silo/cli/core/pkg/aspect/root/flags",
     visibility = ["//visibility:public"],

cli/core/pkg/aspect/root/flags/utils.go
@@ -1,0 +1,33 @@
+/*
+ * Copyright 2022 Aspect Build Systems, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package flags
+
+func AddFlagToCommand(command []string, flag string) []string {
+	result := make([]string, 0, len(command)+1)
+	for i, c := range command {
+		if c == "--" {
+			// inject the flag right before a double dash if it exists
+			result = append(result, flag)
+			result = append(result, command[i:len(command)]...)
+			return result
+		}
+		result = append(result, c)
+	}
+	// if no double dash then add the flag at the end of the command
+	result = append(result, flag)
+	return result
+}

cli/core/pkg/aspect/run/BUILD.bazel
@@ -6,6 +6,7 @@ go_library(
     importpath = "github.com/aspect-build/silo/cli/core/pkg/aspect/run",
     visibility = ["//visibility:public"],
     deps = [
+        "//cli/core/pkg/aspect/root/flags",
         "//cli/core/pkg/aspecterrors",
         "//cli/core/pkg/bazel",
         "//cli/core/pkg/ioutils",

cli/core/pkg/aspect/run/run.go
@@ -20,6 +20,7 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/aspect-build/silo/cli/core/pkg/aspect/root/flags"
 	"github.com/aspect-build/silo/cli/core/pkg/aspecterrors"
 	"github.com/aspect-build/silo/cli/core/pkg/bazel"
 	"github.com/aspect-build/silo/cli/core/pkg/ioutils"
@@ -59,7 +60,7 @@ func (runner *Run) Run(ctx context.Context, _ *cobra.Command, args []string) (ex
 	if bep.HasBESBackend(ctx) {
 		besBackend := bep.BESBackendFromContext(ctx)
 		besBackendFlag := fmt.Sprintf("--bes_backend=%s", besBackend.Addr())
-		bazelCmd = append(bazelCmd, besBackendFlag)
+		bazelCmd = flags.AddFlagToCommand(bazelCmd, besBackendFlag)
 	}
 
 	exitCode, bazelErr := runner.bzl.RunCommand(runner.Streams, nil, bazelCmd...)

cli/core/pkg/aspect/build/build_test.go
@@ -113,7 +113,7 @@ func TestBuild(t *testing.T) {
 		bzl := bazel_mock.NewMockBazel(ctrl)
 		bzl.
 			EXPECT().
-			RunCommand(streams, nil, "build", "//...", "--bes_backend=grpc://127.0.0.1:12345").
+			RunCommand(streams, nil, "build", "--bes_backend=grpc://127.0.0.1:12345", "--", "//...").
 			Return(0, nil)
 		besBackend := bep_mock.NewMockBESBackend(ctrl)
 		besBackend.
@@ -129,7 +129,7 @@ func TestBuild(t *testing.T) {
 		ctx := bep.InjectBESBackend(context.Background(), besBackend)
 
 		b := build.New(streams, bzl)
-		err := b.Run(ctx, nil, []string{"//..."})
+		err := b.Run(ctx, nil, []string{"--", "//..."})
 
 		g.Expect(err).To(BeNil())
 	})

cli/core/pkg/aspect/run/run_test.go
@@ -113,7 +113,7 @@ func TestRun(t *testing.T) {
 		bzl := bazel_mock.NewMockBazel(ctrl)
 		bzl.
 			EXPECT().
-			RunCommand(streams, nil, "run", "//...", "--bes_backend=grpc://127.0.0.1:12345").
+			RunCommand(streams, nil, "run", "//my/runable:target", "--bes_backend=grpc://127.0.0.1:12345", "--", "myarg").
 			Return(0, nil)
 		besBackend := bep_mock.NewMockBESBackend(ctrl)
 		besBackend.
@@ -129,7 +129,7 @@ func TestRun(t *testing.T) {
 		ctx := bep.InjectBESBackend(context.Background(), besBackend)
 
 		b := run.New(streams, bzl)
-		err := b.Run(ctx, nil, []string{"//..."})
+		err := b.Run(ctx, nil, []string{"//my/runable:target", "--", "myarg"})
 
 		g.Expect(err).To(BeNil())
 	})

cli/core/pkg/aspect/test/BUILD.bazel
@@ -6,6 +6,7 @@ go_library(
     importpath = "github.com/aspect-build/silo/cli/core/pkg/aspect/test",
     visibility = ["//visibility:public"],
     deps = [
+        "//cli/core/pkg/aspect/root/flags",
         "//cli/core/pkg/aspecterrors",
         "//cli/core/pkg/bazel",
         "//cli/core/pkg/ioutils",

cli/core/pkg/aspect/test/test.go
@@ -20,6 +20,7 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/aspect-build/silo/cli/core/pkg/aspect/root/flags"
 	"github.com/aspect-build/silo/cli/core/pkg/aspecterrors"
 	"github.com/aspect-build/silo/cli/core/pkg/bazel"
 	"github.com/aspect-build/silo/cli/core/pkg/ioutils"
@@ -52,7 +53,7 @@ func (runner *Test) Run(ctx context.Context, _ *cobra.Command, args []string) (e
 	if bep.HasBESBackend(ctx) {
 		besBackend := bep.BESBackendFromContext(ctx)
 		besBackendFlag := fmt.Sprintf("--bes_backend=%s", besBackend.Addr())
-		bazelCmd = append(bazelCmd, besBackendFlag)
+		bazelCmd = flags.AddFlagToCommand(bazelCmd, besBackendFlag)
 	}
 
 	exitCode, bazelErr := runner.bzl.RunCommand(runner.Streams, nil, bazelCmd...)