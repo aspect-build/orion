aspect-build/orion

commit 4eefc7953861b1cdaaf0fdcddc2aa47a6c817033

perf(cli): parse and cache orion queries on declaration (#7287)
.. instead of re-checking the cache on every execution of a query. This
reduces locking for cache check+write on every query execution

---

### Changes are visible to end-users: yes

- Searched for relevant documentation and updated as needed: yes
- Breaking change (forces users to change their own code or config): no
- Suggested release notes appear below: yes

Reduce locking during concurrent orion query execution.

### Test plan

- Covered by existing test cases

cli/core/gazelle/js/parser/parser.go
@@ -25,9 +25,9 @@ type ParseResult struct {
 
 // Queries finding import statements, tagging such Nodes as 'from' captures.
 // Optionally filtering captures using 'equals-{name}' vars and #eq? statements.
-var importQueries = map[string]string{
+var importQueries = map[string]treeutils.TreeQuery{
 	// Dynamic `import("...")` statement
-	"dynamic_esm_import": `
+	"dynamic_esm_import": treeutils.GetQuery(treeutils.Typescript, `
 		(call_expression
 			function: (import)
 			arguments: (
@@ -36,10 +36,10 @@ var importQueries = map[string]string{
 				)
 			)
 		)
-	`,
+	`),
 
 	// CJS `require("...")` statement
-	"require": `
+	"require": treeutils.GetQuery(treeutils.Typescript, `
 		(call_expression
 			function: (identifier) @equals-require
 			arguments: (
@@ -50,7 +50,7 @@ var importQueries = map[string]string{
 
 			(#eq? @equals-require "require")
 		)
-	`,
+	`),
 }
 
 var tripleSlashRe = regexp.MustCompile(`^///\s*<reference\s+(?:lib|path|types)\s*=\s*"(?P<lib>[^"]+)"`)

cli/core/gazelle/common/treesitter/parser.go
@@ -45,12 +45,12 @@ type ASTQueryResult interface {
 }
 
 type AST interface {
-	Query(query string) <-chan ASTQueryResult
+	Query(query TreeQuery) <-chan ASTQueryResult
 	QueryErrors() []error
 
 	// Wrapper utils
 	// TODO: delete
-	QueryStrings(query, returnVar string) []string
+	QueryStrings(query TreeQuery, returnVar string) []string
 	RootNode() *sitter.Node
 }
 type treeAst struct {

cli/core/gazelle/common/treesitter/queries.go
@@ -12,10 +12,13 @@ import (
 
 var ErrorsQuery = `(ERROR) @error`
 
+type TreeQuery interface {
+}
+
 // A cache of parsed queries per language
 var queryCache = sync.Map{}
 
-func parseQuery(lang LanguageGrammar, queryStr string) *sitterQuery {
+func GetQuery(lang LanguageGrammar, queryStr string) *sitterQuery {
 	key := string(lang) + ":" + queryStr
 
 	q, found := queryCache.Load(key)
@@ -26,11 +29,11 @@ func parseQuery(lang LanguageGrammar, queryStr string) *sitterQuery {
 }
 
 // Run a query finding string query matches.
-func (tree *treeAst) QueryStrings(query, returnVar string) []string {
+func (tree *treeAst) QueryStrings(query TreeQuery, returnVar string) []string {
 	rootNode := tree.sitterTree.RootNode()
 	results := make([]string, 0, 5)
 
-	sitterQuery := parseQuery(tree.lang, query)
+	sitterQuery := query.(*sitterQuery)
 
 	// Execute the query.
 	qc := sitter.NewQueryCursor()
@@ -71,8 +74,8 @@ func (qr queryResult) Captures() map[string]string {
 	return qr.QueryCaptures
 }
 
-func (tree *treeAst) Query(query string) <-chan ASTQueryResult {
-	q := parseQuery(tree.lang, query)
+func (tree *treeAst) Query(query TreeQuery) <-chan ASTQueryResult {
+	q := query.(*sitterQuery)
 	rootNode := tree.sitterTree.RootNode()
 
 	out := make(chan ASTQueryResult)
@@ -160,7 +163,7 @@ func (tree *treeAst) QueryErrors() []error {
 
 	errors := make([]error, 0)
 
-	query := parseQuery(tree.lang, ErrorsQuery)
+	query := GetQuery(tree.lang, ErrorsQuery)
 
 	// Execute the import query
 	qc := sitter.NewQueryCursor()

cli/core/gazelle/common/treesitter/query.go
@@ -12,6 +12,8 @@ type sitterQuery struct {
 	predicatePatterns [][][]sitter.QueryPredicateStep
 }
 
+var _ TreeQuery = (*sitterQuery)(nil)
+
 func mustNewQuery(lang LanguageGrammar, query string) *sitterQuery {
 	q := mustNewTreeQuery(lang, query)
 

cli/pro/gazelle/host/plugin/queries.go
@@ -80,7 +80,7 @@ func NewQueryMatches(matches []QueryMatch) QueryMatches {
 
 type AstQueryParams struct {
 	Grammar treesitter.LanguageGrammar
-	Query   string
+	Query   treesitter.TreeQuery
 }
 
 type RegexQueryParams = string

cli/pro/gazelle/host/starzelle/sdk.go
@@ -111,12 +111,14 @@ func newAstQuery(_ *starlark.Thread, b *starlark.Builtin, args starlark.Tuple, k
 		return nil, err
 	}
 
+	grammar := treesitter.LanguageGrammar(grammarValue.GoString())
+
 	return plugin.QueryDefinition{
 		Filter:    readQueryFilters(filterValue),
 		QueryType: plugin.QueryTypeAst,
 		Params: plugin.AstQueryParams{
-			Grammar: treesitter.LanguageGrammar(grammarValue.GoString()),
-			Query:   query.GoString(),
+			Grammar: grammar,
+			Query:   treesitter.GetQuery(grammar, query.GoString()),
 		},
 	}, nil
 }

cli/pro/gazelle/plugins/bzl.lang.star
@@ -17,6 +17,7 @@ def prepare(_):
         ],
         queries = {
             "loads": aspect.AstQuery(
+                grammar = "starlark",
                 query = """(module
                     (expression_statement
                         (call 

cli/pro/gazelle/tests/starzelle/query-js/imports.lang.star
@@ -11,6 +11,7 @@ def prepare(_):
         ],
         queries = {
             "imports": aspect.AstQuery(
+                grammar = "typescript",
                 filter = "**/*.js",
                 query = "(import_statement (string (string_fragment) @imp))",
             ),

commit ffa38fa038a439f14e5a06fe6939e54f7bb04f2d

fix(cli): tsx file dependencies from type-only dynamic esm imports (#7645)
The query language should be the same as the parsing language, so we
can't parse the queries only once for "typescript".

This was broken in 4eefc7953861b1cdaaf0fdcddc2aa47a6c817033 which
stopped parsing the queries per-file. This fix now invokes `GetQuery`
per-file but will still hit the cache all but the first time for each
file extension.

Overall this change adds a `sync.Map.Get` call per-file, but that is
still significantly better then tree-sitter reparsing the same queries
per-file like before 4eefc7953861b1cdaaf0fdcddc2aa47a6c817033.

---

### Changes are visible to end-users: yes

- Searched for relevant documentation and updated as needed: yes
- Breaking change (forces users to change their own code or config): no
- Suggested release notes appear below: yes

Fix finding dependencies in esm dynamic type-only imports in tsx files.

### Test plan

- New test cases added

cli/core/gazelle/js/parser/parser.go
@@ -25,9 +25,9 @@ type ParseResult struct {
 
 // Queries finding import statements, tagging such Nodes as 'from' captures.
 // Optionally filtering captures using 'equals-{name}' vars and #eq? statements.
-var importQueries = map[string]treeutils.TreeQuery{
+var importQueries = map[string]string{
 	// Dynamic `import("...")` statement
-	"dynamic_esm_import": treeutils.GetQuery(treeutils.Typescript, `
+	"dynamic_esm_import": `
 		(call_expression
 			function: (import)
 			arguments: (
@@ -36,10 +36,10 @@ var importQueries = map[string]treeutils.TreeQuery{
 				)
 			)
 		)
-	`),
+	`,
 
 	// CJS `require("...")` statement
-	"require": treeutils.GetQuery(treeutils.Typescript, `
+	"require": `
 		(call_expression
 			function: (identifier) @equals-require
 			arguments: (
@@ -50,7 +50,7 @@ var importQueries = map[string]treeutils.TreeQuery{
 
 			(#eq? @equals-require "require")
 		)
-	`),
+	`,
 }
 
 // Note that we intentionally omit "lib" here since these directives do not result in a separate dependency
@@ -122,7 +122,9 @@ func ParseSource(filePath string, sourceCode []byte) (ParseResult, []error) {
 		}
 
 		// Extra queries for more complex import statements.
-		for key, q := range importQueries {
+		for key, queryString := range importQueries {
+			q := treeutils.GetQuery(lang, queryString)
+
 			queryResults := tree.QueryStrings(q, "from")
 
 			if len(queryResults) > 0 {

cli/core/gazelle/js/tests/npm_simple_deps/dynamic_import_tsx/BUILD.out
@@ -1,0 +1,11 @@
+load("@aspect_rules_ts//ts:defs.bzl", "ts_project")
+
+ts_project(
+    name = "dynamic_import_tsx",
+    srcs = ["d.tsx"],
+    deps = [
+        "//:node_modules/@aspect-test/c",
+        "//:node_modules/@types/jquery",
+        "//:node_modules/jquery",
+    ],
+)

cli/core/gazelle/js/tests/npm_simple_deps/dynamic_import_tsx/d.tsx
@@ -1,0 +1,5 @@
+import('jquery').then(($) =>
+    import('@aspect-test/c').then((c) => {
+        console.log($(c));
+    })
+);

cli/core/gazelle/js/tests/npm_simple_deps/dynamic_type_import_tsx/BUILD.out
@@ -1,0 +1,17 @@
+load("@aspect_rules_ts//ts:defs.bzl", "ts_project")
+
+ts_project(
+    name = "dynamic_type_import_tsx",
+    srcs = ["type-imports.tsx"],
+    deps = [
+        "//:node_modules/@aspect-test/a",
+        "//:node_modules/@aspect-test/b",
+        "//:node_modules/@aspect-test/c",
+        "//:node_modules/@aspect-test/d",
+        "//:node_modules/@aspect-test/e",
+        "//:node_modules/@aspect-test/f",
+        "//:node_modules/@aspect-test/g",
+        "//:node_modules/@types/jquery",
+        "//:node_modules/jquery",
+    ],
+)

cli/core/gazelle/js/tests/npm_simple_deps/dynamic_type_import_tsx/type-imports.tsx
@@ -1,0 +1,27 @@
+// typeof-import in an argument
+function x<T>(a: typeof import('jquery')): T {
+    return a as T;
+}
+
+// typeof-import in a variable declaration
+export const F: typeof import('@aspect-test/a') = null as any;
+
+// typeof-import in a function invocation
+const f = x<typeof import('@aspect-test/b')>(null);
+
+// typeof-importi n an 'as X' expression
+const g = null as any as typeof import('@aspect-test/c');
+
+// typeof-import in another function invocation
+(async function () {
+    return await x<typeof import('@aspect-test/d')>();
+})();
+
+// typeof-import in a variable declaration
+new Set<typeof import('@aspect-test/e')>();
+
+// type-of in export *
+export type * as Foo from '@aspect-test/f';
+
+// type-of in import *
+import type * as Bar from '@aspect-test/g';